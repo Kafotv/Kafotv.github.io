553e5acfd60ab57d23b387f31382677dffc228bd
Kafotv <Kofotv@hotmail.com>
2026-02-11 22:06:47 +0200

    Update kids.js

--- kids.js
+++ kids.js
@@ -507,10 +507,11 @@ function injectBranding() {
         const branding = document.createElement('div');
         branding.className = 'plyr-branding';
         branding.innerHTML = `
-            <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3">
-                <path d="M2 8V6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2h-16a2 2 0 0 1-2-2v-2"></path>
-                <path d="M2 12h10"></path>
-                <path d="m9 15 3-3-3-3"></path>
+            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
+                <circle cx="12" cy="12" r="10"></circle>
+                <path d="M8 14s1.5 2 4 2 4-2 4-2"></path>
+                <line x1="9" y1="9" x2="9.01" y2="9"></line>
+                <line x1="15" y1="9" x2="15.01" y2="9"></line>
             </svg>
             <span>كفومنك</span>
         `;
